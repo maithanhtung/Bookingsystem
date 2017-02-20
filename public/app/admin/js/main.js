315581f70723e4bdc556e6addcc9e73474408ffd
Le Thuy Hang <thuyhangle.le@gmail.com>
2017-02-20 07:11:30 +0200

    Merge Admin layouts

--- public/app/admin/js/main.js
+++ public/app/admin/js/main.js
@@ -1,5 +1,26 @@
 'use strict';
 
+
+// var Selectize = (function () {
+//     function Selectize() {
+//         this.init();
+//     }
+//     Selectize.prototype.init = function () {
+//         var initValue;
+//         $('.action-box').selectric({
+//             onInit: function (element) {
+//                 initValue = $(this).val();
+//             },
+//             onChange: function (element) {
+//                 if ($(this).val() !== initValue)
+//                     $(element).parents('form').submit();
+//             }
+//         });
+//     };
+//     return Selectize;
+// }());
+// new Selectize();
+
 (function() {
 
 class MainController {
@@ -133,7 +154,6 @@ return a;
  
 
 }
-
 
 
 
